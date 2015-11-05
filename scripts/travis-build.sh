c19fcec206d999c9082772333ac01a9303505f66
Benoit Louy <benoit.louy@fastmail.com>
2015-11-05 12:44:12 -0500

    fix code coverage

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sbt_command="./sbt ++$TRAVIS_SCALA_VERSION ';build;publishLocal'"
-eval $sbt_command
+sbt_prefix="./sbt ++$TRAVIS_SCALA_VERSION"
+
+check_command="$sbt_prefix build && bash <(curl -s https://codecov.io/bash)"
+publish_command="$sbt_prefix publishLocal"
+
+eval $check_command
+eval $publish_command
